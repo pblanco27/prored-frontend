8f484bbc3961dba51b325a8c5c86c89f23b37ed6
Paolo Blanco <pblanco27@hotmail.es>
2020-03-16 20:25:35 -0600

    Se arregla el problema del envío del formulario

--- src/components/Modal/ModalRed.js
+++ src/components/Modal/ModalRed.js
@@ -1,46 +1,36 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 
-
-
 export default class ModalRed extends Component {
     constructor(props) {
         super(props);
         this.state = {
             name: '',
             type: '',
-    
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChangeName = this.handleChangeName.bind(this);
         this.handleChangeType = this.handleChangeType.bind(this);
     }  
 
-    
-
     handleChangeName = event => {
         this.setState({ name: event.target.value });
-        
     }
 
     handleChangeType = event => {
         this.setState({ type: event.target.value });
     }
 
-    handleSubmit = async (event) =>  {
+    handleSubmit = async event =>  {
         event.preventDefault();
         const network = {
             name: this.state.name,
             type : this.state.type, 
         };
-        console.log(network);
         const resData = await axios.post(`/network`, network )
-        console.log(resData.data);
         this.setState({ type: '' , name : ''});
         this.props.fun();
-      
     } 
-    
 
     render() {
         return (
@@ -57,8 +47,8 @@ export default class ModalRed extends Component {
                                 <form onSubmit={this.handleSubmit}>
                                     <div class="form-group">
                                         <label for="tipoRed">Tipo de red</label>
-                                        <select class="form-control" name="name" onChange={this.handleChangeType} value = {this.state.type}>
-                                            <option class="select-cs" value="" label="Seleccione la red" selected="selected">Seleccione la red</option>
+                                        <select class="form-control" name="type" required onChange={this.handleChangeType} value = {this.state.type}>
+                                            <option class="select-cs" value="" defaultValue>Seleccione la red</option>
                                             <option value="Municipalidad">Municipalidad</option>
                                             <option value="ONG">ONG</option>
                                             <option value="Asociaciones">Asociaciones</option>
@@ -67,12 +57,11 @@ export default class ModalRed extends Component {
                                     </div>
                                     <div class="form-group">
                                         <label for="nombreRed">Nombre</label>
-                                        <input class="form-control" type="text" value= {this.state.name} name="type" id="nombreRed" required onChange={this.handleChangeName}></input>
+                                        <input class="form-control" type="text" value= {this.state.name} name="name" id="nombreRed" required onChange={this.handleChangeName}></input>
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-                                        <input type="submit" class="btn btn-primary" data-dismiss="modal" value="Aceptar" onClick = {this.handleSubmit} />
-                                      
+                                        <input type="submit" class="btn btn-primary" value="Aceptar"/>
                                     </div>
                                 </form>
                             </div>
