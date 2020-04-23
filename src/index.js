530199d6bc363fd877375461605dc6f42808bf39
Jair Cordero <cordero1823.cr@gmail.com>
2020-04-22 19:09:43 -0600

    Se mueve el BrowserRouter al index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./App.css";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import { BrowserRouter as Router } from "react-router-dom";
 // import $ from 'jquery';
 // import Popper from 'popper.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
-
-
-
-
